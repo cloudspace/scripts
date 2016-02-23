1844bc29a889ec2191347c96a3b474bb6ce8b3a2
Chris Moore <chris@cloudspace.com>
2016-02-23 09:58:34 -0500

    adding the recursive flag to fetch nested submodule commits as well

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-git submodule update --init
+git submodule update --init --recursive
 git submodule foreach '(git checkout master && git pull --rebase) || echo "Not attempting to sync"'
 if [ ".$@" == "." ]; then
     ./mvnw clean install -P build,!base,!starters,!apps -DskipTests
